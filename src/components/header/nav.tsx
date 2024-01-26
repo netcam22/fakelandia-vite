2d3f5e01dee9b2c61190b0ee05379961cc9ab864
netcam22 <annettelesage@btinternet.com>
2024-01-26 23:26:25 +0000

    feat: implement burger menu

--- src/components/header/nav.tsx
+++ src/components/header/nav.tsx
@@ -1,12 +1,15 @@
 import { NavLink } from "react-router-dom";
 
-const Nav = () => <nav className = "navbar">
+const Nav = () => <nav className = "navbar navbar--style">
     <div className = "navbar__element">
-    <div className = "navbar__element--bars"><i className="fa fa-bars"></i></div>
-    <ul className = "navbar__list navbar__list--light">
-        <li className = "navbar__item navbar__item--dark"><NavLink to = "/">Home</NavLink></li>
-        <li className = "navbar__item navbar__item--dark"><NavLink to = "/misdemeanours">Misdemeanours</NavLink></li>
-        <li className = "navbar__item navbar__item--dark"><NavLink to = "/confession">Confess To Us</NavLink>
+    <input className = "navbar__checkbox" type="checkbox" />
+        <span className = "navbar__burger navbar__burger--color"></span>
+        <span className = "navbar__burger navbar__burger--color"></span>
+        <span className = "navbar__burger navbar__burger--color"></span>
+    <ul className = "navbar__list navbar__list--style navbar__list--transition">
+        <li className = "navbar__item navbar__item--style"><NavLink to = "/">Home</NavLink></li>
+        <li className = "navbar__item navbar__item--style"><NavLink to = "/misdemeanours">Misdemeanours</NavLink></li>
+        <li className = "navbar__item navbar__item--style"><NavLink to = "/confession">Confess To Us</NavLink>
         </li>
     </ul>
     </div>
